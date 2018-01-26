2995d345fde646dce9970407e757f760d2b9e9af
dlicul <dlicul@ancestry.com>
2018-01-26 15:11:37 -0700

    fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone https://github.com/gmarik/vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/dim13/smyck.vim.git ~/.vim
+mkdir -p ~/.vim/colors
+curl https://raw.githubusercontent.com/dim13/smyck.vim/master/colors/smyck.vim -o ~/.vim/colors/smyck.vim
 git clone https://github.com/dlicul/dotvim.git ~/dotvim
 ln -sf ~/dotvim/.vimrc ~/.vimrc
 
